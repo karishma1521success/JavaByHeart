97f3064201e5e6a738bede13e850f200c3c4a6de
karishma gupta <karishma15102002@gmail.com>
2023-11-02 14:29:09 +0530

    Added the program of reverse the number using iterative method in java

--- Questions/loop/reverseNumber.java
+++ Questions/loop/reverseNumber.java
@@ -5,8 +5,32 @@ public class reverseNumber {
         Scanner scn = new Scanner(System.in);
         System.out.println("Enter the Number");
         int number = scn.nextInt();
-        int copyOfNumber = number;
         //first we have to count the total number of digits so the we can create a reverse number for that we need the multiples of 10
+        int totalDigits =  countDigits(number);
+        System.out.println(totalDigits);
+        int reversedNumber  = reversedDigits(number);
+        System.out.println("The reversed number of "+number+ " is " + reversedNumber);
+        scn.close();
         
     } 
+
+    public static int countDigits(int number){
+        int count = 0;
+        while(number != 0){
+            number = number/10;
+            count++;
+        }
+        return count;
+    }
+
+    //optimal solution - can reversed the number without count of digits.
+    public static int reversedDigits(int number){
+        int reverseNumber = 0;
+        while(number != 0){
+            int digit = number % 10;
+            reverseNumber = (reverseNumber * 10) + digit;
+            number = number / 10 ;
+        }
+        return reverseNumber;
+    }
 }
