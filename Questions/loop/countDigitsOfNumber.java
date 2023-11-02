4fac0ba84b24b39bf03c1ebce81de3889847b72f
karishma gupta <karishma15102002@gmail.com>
2023-11-02 14:14:52 +0530

    Added the program of count number of digits in java using iterative method

--- Questions/loop/countDigitsOfNumber.java
+++ Questions/loop/countDigitsOfNumber.java
@@ -1,3 +1,17 @@
+import java.util.*;
+
 public class countDigitsOfNumber {
-    
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int number = scn.nextInt();
+        int copyOfNumber = number;
+        int count = 0;
+        while(copyOfNumber != 0){
+            copyOfNumber = copyOfNumber/10;
+            count++;
+        }
+        System.out.println("The Number of Digits in " + number + " is " + count);
+        scn.close();
+    }    
 }
---
+++ Questions/loop/reverseNumber.java
@@ -0,0 +1,12 @@
+import java.util.*;
+//Take a number input and reverse that input number    398 --- 893
+public class reverseNumber {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int number = scn.nextInt();
+        int copyOfNumber = number;
+        //first we have to count the total number of digits so the we can create a reverse number for that we need the multiples of 10
+        
+    } 
+}
