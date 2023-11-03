6a75210b68e10e80d760885104fe5e1ce088b4d1
karishma gupta <karishma15102002@gmail.com>
2023-11-03 18:37:09 +0530

    updated

---
+++ Main.java
@@ -0,0 +1,9 @@
+// import java.util.*;
+
+public class Main { 
+    //class name should be same as file name and the name should be in pascal case convention.
+    public static void main(String[] args){
+        System.out.println("java code starts from here");
+    }
+    
+}
