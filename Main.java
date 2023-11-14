5a0423d8f84950f51d6c32b914535b99ac3f063f
karishma gupta <karishma15102002@gmail.com>
2023-11-14 16:07:21 +0530

    updated

--- Main.java
+++ Main.java
@@ -4,6 +4,8 @@ public class Main {
     //class name should be same as file name and the name should be in pascal case convention.
     public static void main(String[] args){
         System.out.println("java code starts from here");
+        int a= 5,b=0;
+        System.out.println(a/b);
     }
     
 }
